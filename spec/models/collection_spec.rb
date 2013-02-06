a2903d96be454806a039c43a73925c7526734c25
Carolyn Cole <cam156@psu.edu>
2013-02-06 09:51:50 -0500

    implemented the rest of the model tests

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -33,7 +33,7 @@ describe Collection do
     @gf2.save
   end
   after(:each) do
-    @collection.destroy
+    @collection.destroy rescue
     @gf1.destroy
     @gf2.destroy
   end
@@ -74,9 +74,27 @@ describe Collection do
     @collection.save
     @collection.date_modified.should == modified_date
   end
-  it "should have a title"
-  it "should have a description"
-  it "should have the expected display terms"
-  it "should have the expected edit terms"
-  it "should not delete member files when deleted"
+  it "should have a title" do
+    @collection.title = "title"
+    @collection.save
+    Collection.find(@collection.pid).title.should == @collection.title
+  end
+  it "should have a description" do
+    @collection.description = "description"
+    @collection.save
+    Collection.find(@collection.pid).description.should == @collection.description
+  end
+  it "should have the expected display terms" do
+    @collection.terms_for_display.should == [:title, :description, :date_uploaded, :date_modified]
+  end
+  it "should have the expected edit terms" do
+    @collection.terms_for_editing.should == [:title,:description]
+  end
+  it "should not delete member files when deleted" do
+    @collection.generic_files = [@gf1, @gf2]
+    @collection.save
+    @collection.destroy
+    lambda {GenericFile.find(@gf1.pid)}.should_not raise_error ActiveFedora::ObjectNotFoundError
+    lambda {GenericFile.find(@gf2.pid)}.should_not raise_error ActiveFedora::ObjectNotFoundError
+  end
 end
